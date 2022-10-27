78efa659ed87e6d045f62a3f57eb3c6e4f4108fa
LavrinovichA <mozo@bk.ru>
2022-10-27 13:25:13 +0300

    fix bug

--- year.py
+++ year.py
@@ -1,35 +1,33 @@
-def is_number(y):
+def is_number(str):
     try:
-        int(y)
+        int(str)
         return True
     except ValueError:
         return False
 while True:
-    y = (input('Введите год \n'))
     while True:
+        y = (input('Введите год \n'))
         if is_number(y) == True :
             flag = 0
             year = int(y)
-            break
         else:
             print('Не корректное значение\n')
             flag = 1
+        if flag == 0:
             break
-    if flag == 0:
-        break
-if year % 4 == 0:
-    print('Год високосный\n')
-else:
-    print('Не високосный год\n')
-while True:
-    y=input('Хотите продолжить? (Y/N)\n')
-    if y=='n' or y == 'N':
-        flag = 0
-        break
-    elif y == 'y' or y == 'Y':
-        flag = 1 
-        break
+    if year % 4 == 0:
+        print('Год високосный\n')
     else:
-        print('Не корректное значение\n')
+        print('Не високосный год\n')
+    while True:
+        y=input('Хотите продолжить? (Y/N)\n')
+        if y=='n' or y == 'N':
+            flag = 0
+            break
+        elif y == 'y' or y == 'Y':
+            flag = 1 
+            break
+        else:
+            print('Не корректное значение\n')
     if flag == 0:
         break
